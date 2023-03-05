7942e0a0d8dfa32a20741a38ed9cc22f357f1cc7
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-03-05 10:40:24 +0200

    Add markdown-preview plugin

--- lua/bbojan/packer.lua
+++ lua/bbojan/packer.lua
@@ -51,4 +51,13 @@ use {
   }
 }
 
+-- install without yarn or npm
+use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+})
+
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+
 end)
