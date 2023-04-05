b7b66f9df09f84740bbb1ee14e4afd06c956799e
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-04-05 10:49:03 +0300

    Setup golangci in lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -67,6 +67,25 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+-- golangci-lint-langserver
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig/configs'
+
+if not configs.golangcilsp then
+ 	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+					command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json", "--issues-exit-code=1" };
+			}
+		};
+	}
+end
+lspconfig.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'}
+}
+
 lsp.setup()
 
 vim.diagnostic.config({
