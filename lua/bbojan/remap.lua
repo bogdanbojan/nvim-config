ffffa97cf848d2502e2a31b7d0a61c4fdf24f120
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-06-09 04:27:09 +0000

    Add remap for find and replace in visual mode

--- lua/bbojan/remap.lua
+++ lua/bbojan/remap.lua
@@ -30,6 +30,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("v", "<leader>s", "xnoremap ; y<Esc>:%s/<C-R>\"//g<Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = false })
 
 vim.keymap.set("n", "<leader>+", "<C-a>")
